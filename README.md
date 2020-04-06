956990014cb89d6fa75b91e255fb1b75d09d1663
root <liushaoweihua@126.com>
2020-04-06 20:09:43 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# SpringerDownloader
+
+## 简介
+Springer Nature为教育界和学术界人士免费提供基础教科书
+随着冠状病毒的爆发对教育产生了前所未有的影响，Springer Nature 将免费提供各学科一系列基础教科书，以支持世界各地的高等教育机构。施普林格-自然（Springer Nature）正在发起一项全球计划，支持全球高等教育机构的学习和教学。这将使各机构能够免费获得施普林格自然公司电子书系列中的 500 多本重要教科书。这些书籍将免费对外公开下载。截止日期为 7 月底。
+
+Springer Nature 已经免费提供了数千篇关于冠状病毒的研究文章( https://www.springernature.com/gp/researchers/campaigns/coronavirus ) ， 以满足人们获取基本研究的需求，这项新举措更进一步支持教学工作，让学生、学者和大学教职员工能够访问 Springer Nature 的基础教科书。
+
+Springer Nature Books 的总经理 Niels Peter Thomas 在评论时说。"随着冠状病毒危机在全球范围内的影响不断加剧，远程获取教育资源已经变得至关重要。我们希望在这一充满挑战的时期为教师和学生提供支持，并希望这一举措能在一定程度上帮助到大家，500 多本关键的教科书将在网上免费提供。"
+
+除此计划外，斯普林格-自然出版社的教育部门 Macmillan International Higher Education 还采取了一系列举措，以支持学术界和学生在这个充满挑战的时代进行教学和学习。对于 MIHE 的书目，学术界人士可以在已经有印刷版的情况下申请电子书样书，并可以开放使用其课程管理系统的书籍。MIHE 还与 VitalSource、Kortext 和 RedShelf 等区域性供应商合作，以确保学生可以获得电子书，帮助他们继续远程学习。
+
+所有书目和下载连接:
+https://resource-cms.springernature.com/springer-cms/rest/v1/content/17858272/data/v3
+
+## 使用
+1. 先安装依赖项：pip install -r requirements.txt -i https://pypi.douban.com/simple
+2. 下载：python springer_downloader.py
---
+++ springer_downloader.py
@@ -0,0 +1,57 @@
+import os
+import time
+import codecs
+import requests
+import pandas as pd
+
+
+class SpringerDownloader:
+    
+    def __init__(self):
+        self.df = [[i[0], i[1].split(";")[0], i[2]] for i in pd.read_excel("free_english_textbook.xlsx")[["Book Title", "Subject Classification", "OpenURL"]].values.tolist()]
+        self.current_path = os.getcwd()
+    
+    def wget(self, url, save_path=None, rename=None):
+        file_name = url[url.rfind("/")+1:]
+        if not save_path:
+            save_path = self.current_path
+        if not rename:
+            rename = file_name
+        save_path = os.path.abspath(os.path.join(save_path, rename))
+        print("[wget]   downloading from {}".format(url))
+        start = time.time()
+        size = 0
+        response = requests.get(url, stream=True)
+        chunk_size = 10240
+        content_size = int(response.headers["content-length"])
+        if response.status_code == 200:
+            print("[wget]   file size: %.2f MB" %(content_size / 1024 / 1024))
+            with codecs.open(save_path, "wb") as f:
+                for data in response.iter_content(chunk_size=chunk_size):
+                    f.write(data)
+                    size += len(data)
+                    print("\r"+"[wget]   %s%.2f%%"
+                          %(">"*int(size*50/content_size), float(size/content_size*100)), end="")
+        end = time.time()
+        print("\n"+"[wget]   complete! cost: %.2fs."%(end-start))
+        print("[wget]   save at: %s\n" %save_path)
+        
+    def mkdir(self, file_dir):
+        file_dir = os.path.abspath(os.path.join(self.current_path, file_dir))
+        os.makedirs(file_dir)
+        print("[mkdir]  create directory {}".format(file_dir))
+    
+    def download(self):
+        for _, i in enumerate(self.df):
+            print("Downloading 【{}】. \nCurrent #books: {}, total #books: {}".format(i[0], _ + 1, len(self.df)))
+            file_name = i[0] + ".pdf"
+            classification_dir = os.path.abspath(os.path.join(self.current_path, i[1]))
+            if not os.path.exists(classification_dir):
+                self.mkdir(classification_dir)
+            file_url = "https://link.springer.com/content/pdf/" + requests.get(i[2]).url.split("/")[-1] + ".pdf"
+            self.wget(file_url, save_path=classification_dir, rename=file_name)
+            
+            
+if __name__ == "__main__":
+    sd = SpringerDownloader()
+    sd.download()
